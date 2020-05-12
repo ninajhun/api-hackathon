7eda55e83d8c4ff3e931230c6b319d110834b03d
ninajhun <ninajhun@berkeley.edu>
2020-05-12 15:47:00 -0700

    random desination city added to ajax call

--- components/flight-info.js
+++ components/flight-info.js
@@ -1,23 +1,56 @@
 
 var originAirport;
-// var randomAirport;
+var randomAirport;
 var flightURL;
 
 var startingCity = document.getElementById("starting-city")   //get starting city value from form
-startingCity.addEventListener("change", getStartingCity);
+startingCity.addEventListener("change", getFlightInfo);
 
-function getStartingCity() {
+
+randomAirportArray = ["ATL", "PEK", "LAX", "HND", "DXB", "ORD", "LHR", "PVG", "CDG", "DFW"]
+
+
+function shuffle(array) {
+  for (var i = 0; i < array.length; i++) {
+    var randomPosition = Math.floor(Math.random() * array.length);
+    var placeHolder = array[i];
+    array[i] = array[randomPosition];
+    array[randomPosition] = placeHolder;
+  }
+}
+
+function getRandomCity(array) {
+  var updatedArray = [];
+  for (var index = 0; index < array.length; index++){
+  updatedArray.push(array[index] + "-sky");
+  }
+
+  shuffle(updatedArray);
+  randomAirport = updatedArray[0];
+  return randomAirport;
+}
+
+function getStartingCity(){
   document.querySelector(".start-modal").classList.add("hidden");
 
   if (startingCity.value === "los-angeles") {
     originAirport = "LAX-sky";
   } else if (startingCity.value === "orange-county") {
     originAirport = "SNA-sky"
-  } else if (startingCity.value === "new-york"){
+  } else if (startingCity.value === "new-york") {
     originAirport = "JFK-sky"
   }
+  return originAirport;
+}
 
-  flightURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" + originAirport + "/LAX-sky/2020-09-01?inboundpartialdate=2020-12-01"
+
+
+function getFlightInfo() {
+
+  getStartingCity();
+  getRandomCity(randomAirportArray);
+
+  flightURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" + originAirport + "/" + randomAirport +"/2020-09-01?inboundpartialdate=2020-12-01"
 
   var settings = {
     "async": true,
