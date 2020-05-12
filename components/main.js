ae86f43270398c89030fb87c33f9a07ac5d85830
ninajhun <ninajhun@berkeley.edu>
2020-05-12 11:35:58 -0700

    get up file structure and basic start modal

---
+++ components/main.js
@@ -0,0 +1,8 @@
+
+
+var startButton = document.getElementById("start-button")
+
+startButton.addEventListener("click", function(){
+  document.querySelector(".start-modal").classList.add("hidden");
+  console.log("clicked!")
+})
