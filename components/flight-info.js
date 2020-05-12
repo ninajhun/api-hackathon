c4940a3c54647e56ebfadd5de05ef1ef82492cfe
ninajhun <ninajhun@berkeley.edu>
2020-05-12 15:20:41 -0700

    originAirport added to ajax flight info request.

--- components/flight-info.js
+++ components/flight-info.js
@@ -1,30 +1,37 @@
 
-//get starting city value from form
+var originAirport;
+// var randomAirport;
+var flightURL;
 
-var startingCity = document.getElementById("starting-city")
-
+var startingCity = document.getElementById("starting-city")   //get starting city value from form
 startingCity.addEventListener("change", getStartingCity);
 
 function getStartingCity() {
   document.querySelector(".start-modal").classList.add("hidden");
-  console.log("clicked!")
-  console.log("starting city", startingCity.value);
-}
 
-
-
-
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/LAX-sky/2020-09-01?inboundpartialdate=2020-12-01",
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-    "x-rapidapi-key": skyKey
+  if (startingCity.value === "los-angeles") {
+    originAirport = "LAX-sky";
+  } else if (startingCity.value === "orange-county") {
+    originAirport = "SNA-sky"
+  } else if (startingCity.value === "new-york"){
+    originAirport = "JFK-sky"
   }
-}
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
+  flightURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" + originAirport + "/LAX-sky/2020-09-01?inboundpartialdate=2020-12-01"
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": flightURL,
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+      "x-rapidapi-key": skyKey
+    }
+  }
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+
+}
