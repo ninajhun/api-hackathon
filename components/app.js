2a8610de104677ce7c0751729f2489af715c651e
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-09-25 23:01:50 +0000

    get new random city from airports.js

--- components/app.js
+++ components/app.js
@@ -4,7 +4,7 @@ class App {
     this.randomAirport = null;
     this.flightURL = null;
     this.startingCity = startingCity;
-    this.randomAirportInfo = randomAirportInfo;
+    this.randomAirportInfo = randomAirportInfo;  //change to airports later
     this.flightTable = flightTable;
     this.newRandomCity = newRandomCity;
     this.handleFlightInfoSuccess = this.handleFlightInfoSuccess.bind(this);
@@ -18,6 +18,29 @@ class App {
   start() {
     this.startingCity.addEventListener("change", this.getFlightInfo);
     this.newRandomCity.onNewCityClick(this.getFlightInfo);
+
+    // console.log(this.randomAirportInfo)
+  }
+
+  getFlightInfo() {
+    this.getStartingCity();
+    this.getRandomCity(this.randomAirportInfo);
+    this.flightURL = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/${this.originAirport}-sky/${this.randomAirport}-sky/2020-12-01?inboundpartialdate=2020-12-01`
+    //need to change outbound date to use Date()
+    // console.log(this.flightURL)
+
+    $.ajax({
+      "async": true,
+      "crossDomain": true,
+      "url": this.flightURL,
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+        "x-rapidapi-key": skyKey
+      },
+      success: this.handleFlightInfoSuccess,
+      error: this.handleFlightInfoError
+    });
   }
 
   getStartingCity() {
@@ -37,15 +60,16 @@ class App {
     var updatedArray = [];
     for (var index = 0; index < array.length; index++) {
       updatedArray.push(array[index]);
-    }
+    }   //is this necessary?
 
     this.shuffle(updatedArray);
-    this.randomAirport = updatedArray[0].airportCode;
+    // this.randomAirport = updatedArray[0].airportCode;
+    this.randomAirport = updatedArray[0].iata_code;
 
-    var header = document.querySelector("header");
-    var headerText = document.querySelector(".random-city-text");
-    headerText.textContent = "Pack your bags, you're going to " + updatedArray[0].city + "!"
-    header.append(headerText);
+    // var header = document.querySelector("header");
+    // var headerText = document.querySelector(".random-city-text");
+    // headerText.textContent = "Pack your bags, you're going to " + updatedArray[0].city + "!"
+    // header.append(headerText);
 
     document.getElementById("map").classList.remove("hidden");
     this.initMap(updatedArray[0].city, updatedArray[0].coords)
@@ -53,26 +77,7 @@ class App {
     return this.randomAirport;
   }
 
-  getFlightInfo() {
-    this.getStartingCity();
-    this.getRandomCity(this.randomAirportInfo);
-    this.flightURL = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/${ this.originAirport}-sky/${this.randomAirport}-sky/2020-12-01?inboundpartialdate=2020-12-01`
-    //need to change outbound date to use Date()
-    console.log(this.flightURL)
 
-    $.ajax({
-      "async": true,
-      "crossDomain": true,
-      "url": this.flightURL,
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-        "x-rapidapi-key": skyKey
-      },
-      success: this.handleFlightInfoSuccess,
-      error: this.handleFlightInfoError
-    });
-  }
 
   handleFlightInfoSuccess(flightInfo) {
     this.flightTable.onStartCityChosen(flightInfo)
