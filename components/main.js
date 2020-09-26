4802078e8e0a0a5cff97f219f4f28010a50ec1cd
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-09-26 02:18:39 +0000

    fix bug where google maps loads before initMap call - remove async defer from script src

--- components/main.js
+++ components/main.js
@@ -10,15 +10,14 @@ var newRandomCity = new NewRandomCity(newCityButton);
 var originAirport;
 var randomAirport;
 var flightURL;
-var startingCity = document.getElementById("starting-city")   //get starting city value from form
+var startingCity = document.getElementById("starting-city")
 
-function initMap(latitude_deg, longitude_deg) {  // Initialize and add the map
-  console.log(typeof(latitude_deg), typeof(longitude_deg))
-  var pos = { lat: latitude_deg, lng: longitude_deg } // The location of city
-  var map = new google.maps.Map(     // The map, centered at city
+function initMap(latitude_deg, longitude_deg) {
+  var pos = { lat: latitude_deg, lng: longitude_deg }
+  var map = new google.maps.Map(
     document.getElementById('map'), { zoom: 6, center: pos });
-  var marker = new google.maps.Marker({ position: pos, map: map });  // The marker, positioned at city
-  console.log("hi")
+  var marker = new google.maps.Marker({ position: pos, map: map });
+
 }
 
 var app = new App(originAirport, randomAirport, flightURL, startingCity, airports, flightTable, newRandomCity, initMap); // initMap
